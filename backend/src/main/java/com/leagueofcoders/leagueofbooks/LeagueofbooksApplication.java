db33f5d481aa10b7ddc3fcc17e3a2b59e2f26eea
cpradaborriol-star <cpradaborriol@gmail.com>
2026-05-21 10:51:40 +0200

    Primer commit

---
+++ backend/src/main/java/com/leagueofcoders/leagueofbooks/LeagueofbooksApplication.java
@@ -0,0 +1,13 @@
+package com.leagueofcoders.leagueofbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueofbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueofbooksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/leagueofcoders/leagueofbooks/LeagueofbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leagueofcoders.leagueofbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueofbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

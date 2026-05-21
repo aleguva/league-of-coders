91b6e30a7c3a6c97874fb0164a590bb9ba984f66
aleguva <alejandroguzmanvasquez@gmail.com>
2026-05-21 13:06:50 +0100

    Revert "se añade el start.bat"
    This reverts commit 1a98fdba32ca63378158ee3b1fc676fca542c4c8.

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Iniciando backend...
-start "Backend" cmd /k "cd backend && mvnw.cmd spring-boot:run"
-
-echo Iniciando frontend...
-start "Frontend" cmd /k "cd frontend && npm start"
-
-echo Backend y frontend iniciados en ventanas separadas.
-echo Cierra las ventanas de Backend y Frontend para detenerlos.
